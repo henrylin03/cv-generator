8d0a520e725a5febc14eaa20bea5b54d51cf9d90
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-09-20 18:11:26 +1000

    feat: build header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,14 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
+import "./styles/global.css";
+import Header from "./components/Header";
 
 function App() {
-  return <MantineProvider></MantineProvider>;
+  return (
+    <MantineProvider>
+      <Header />
+    </MantineProvider>
+  );
 }
 
 export default App;
---
+++ src/components/Header.jsx
@@ -0,0 +1,9 @@
+export default function Header() {
+  return (
+    <header>
+      <a href="/">
+        <h1>CV Generator</h1>
+      </a>
+    </header>
+  );
+}
