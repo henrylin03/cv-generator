0b44846742b065fd238850b9c1ea73538ba42680
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-09-25 10:01:50 +1000

    Revert commit before attempts to implement multipage design.
    This reverts commit 8d0a520e725a5febc14eaa20bea5b54d51cf9d90. Multipage design likely requires routing. Single page design will be built for MVP.

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,8 @@
 import "@mantine/core/styles.css";
 import { MantineProvider } from "@mantine/core";
-import "./styles/global.css";
-import PersonalDetailsForm from "./components/PersonalDetailsForm";
-
-// TODO: use state hook to determine which form will show. use an object as variable, with heading and the component to show. the state will merely be a reference (key in that object)??
 
 function App() {
-  return (
-    <MantineProvider>
-      <header>
-        <a href="/">
-          <h1>CV Generator</h1>
-        </a>
-      </header>
-      <main>
-        <PersonalDetailsForm />
-      </main>
-    </MantineProvider>
-  );
+  return <MantineProvider></MantineProvider>;
 }
 
 export default App;
