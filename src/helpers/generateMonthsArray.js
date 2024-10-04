2e18fbd4d53cc10f94d11f7cb800b4412669d906
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-10-04 11:06:56 +1000

    chore: delete unused helpers
    an array no longer needs to be generated for month picker as we are utilising Mantine's month picker component which handles this for us

--- src/helpers/generateMonthsArray.js
+++
@@ -1,6 +0,0 @@
-const generateMonthsArray = () =>
-  Array.from({ length: 12 }, (_, idx) =>
-    new Date(0, idx).toLocaleString("en-US", { month: "long" })
-  );
-
-export default generateMonthsArray;
--- src/helpers/toCamelCase.js
+++
@@ -1,8 +0,0 @@
-export default function toCamelCase(str) {
-  const lowerCaseFirstLetter = (word, index) =>
-    index === 0 ? word.toLowerCase() : word.toUpperCase();
-
-  return str
-    .replace(/(?:^\w|[A-Z]|\b\w)/g, lowerCaseFirstLetter)
-    .replace(/\s+/g, "");
-}
