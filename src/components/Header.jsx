32beb6d5297b58df5e73be22e3d8291112a058d2
henrylin03 <83106787+henrylin03@users.noreply.github.com>
2024-10-10 11:43:04 +1100

    chore: remove download CV button until #6 is actioned. remove clear all button until #4 is actioned.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,10 @@
-import { Paper, Button } from "@mantine/core";
+import { Paper } from "@mantine/core";
 
 export default function Header() {
   return (
     <header>
       <Paper className="card">
         <h1 className="branding">CV Gen</h1>
-        <div className="button-group">
-          <Button variant="filled" color="#00ccb8" className="primary-button">
-            Download
-          </Button>
-          <Button variant="outline" color="#8490b3" className="destructive-button">
-            Clear all
-          </Button>
-        </div>
       </Paper>
     </header>
   );
