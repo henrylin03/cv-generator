72f2ef6b1a4f83d5b5d33bb97252a7f6d4431dce
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-09-02 23:10:20 +1000

    chore(_root.tsx): remove TanStackRouterDevTools for now.
    not used and adds a weird padding to the bottom of the page

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,10 +1,10 @@
-import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
-import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
+import { createRootRoute, Outlet } from "@tanstack/react-router";
+// import { TanStackRouterDevtools } from "@tanstack/react-router-devtools";
 
 const RootLayout = () => (
 	<>
 		<Outlet />
-		<TanStackRouterDevtools />
+		{/* <TanStackRouterDevtools /> */}
 	</>
 );
 
