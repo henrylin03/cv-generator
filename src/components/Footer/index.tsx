9b38d4b8021728508916d9dee902c4a5d9ab8281
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-10-26 19:58:03 +1100

    chore: install lint-staged and husky such that biome formatter runs on every commit. biome autoformats, and then checks if there are any errors. chore(.vscode): organise imports using biome to avoid issues when saving.

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,20 +1,20 @@
-"use client"
+"use client";
 
-import { usePathname } from "next/navigation";
 import Link from "next/link";
+import { usePathname } from "next/navigation";
 import { getNextStepRoute, getPreviousStepRoute } from "@/appSteps";
 
 const Footer = () => {
-    const currentPath = usePathname();
-    const previousStepRoute = getPreviousStepRoute(currentPath);
-    const nextStepRoute = getNextStepRoute(currentPath)
+	const currentPath = usePathname();
+	const previousStepRoute = getPreviousStepRoute(currentPath);
+	const nextStepRoute = getNextStepRoute(currentPath);
 
-    return (
-        <footer>
-            {previousStepRoute && <Link href={previousStepRoute}>Previous step</Link>}
-            {nextStepRoute && <Link href={nextStepRoute}>Next step</Link>}
-        </footer>
-    );
+	return (
+		<footer>
+			{previousStepRoute && <Link href={previousStepRoute}>Previous step</Link>}
+			{nextStepRoute && <Link href={nextStepRoute}>Next step</Link>}
+		</footer>
+	);
 };
 
 export default Footer;
