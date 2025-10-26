d9736aead49e2699b9920255a8235bd202104e0b
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-10-26 11:15:06 +1100

    feat(Header): add links to navigate to other pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,7 +1,23 @@
+import Link from "next/link";
 import styles from "./Header.module.css"
 
+interface Page {
+    title: string;
+    route: string;
+}
+
 const Header = () => {
-    return <header>this is header</header>
+    const PAGES: Page[] = [{ title: "Contact details", route: "/contact" }, { title: "Education", route: "/education" }, { title: "Experience", route: "/experience" }, { title: "Preview", route: "/preview" }]
+
+    return <header>
+        <nav>
+            <ul>
+                {PAGES.map((page) =>
+                    <li key={page.route}><Link href={page.route}>{page.title}</Link></li>
+                )}
+            </ul>
+        </nav>
+    </header>
 }
 
 export default Header;
