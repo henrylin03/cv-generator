2675808143675956f51adec4fdc673923d339079
Henry Lin <83106787+henrylin03@users.noreply.github.com>
2025-10-26 11:24:07 +1100

    feat: build placeholder pages for all the likely pages (currently), without dynamic subpages, and navigate through them using the navbar in the header
    confirmed working! we're using the app-based routing architecture which IIUC is the newer way of doing things in NextJS

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,31 +2,31 @@ import Link from "next/link";
 import styles from "./Header.module.css";
 
 interface Page {
-    title: string;
-    route: string;
+	title: string;
+	route: string;
 }
 
 const Header = () => {
-    const PAGES: Page[] = [
-        { title: "Contact details", route: "/contact" },
-        { title: "Education", route: "/education" },
-        { title: "Experience", route: "/experience" },
-        { title: "Preview", route: "/preview" },
-    ];
+	const PAGES: Page[] = [
+		{ title: "Contact details", route: "/contact-details" },
+		{ title: "Education", route: "/education" },
+		{ title: "Experience", route: "/experience" },
+		{ title: "Preview", route: "/preview" },
+	];
 
-    return (
-        <header>
-            <nav>
-                <ul>
-                    {PAGES.map((page) => (
-                        <li key={page.route}>
-                            <Link href={page.route}>{page.title}</Link>
-                        </li>
-                    ))}
-                </ul>
-            </nav>
-        </header>
-    );
+	return (
+		<header>
+			<nav>
+				<ul>
+					{PAGES.map((page) => (
+						<li key={page.route}>
+							<Link href={page.route}>{page.title}</Link>
+						</li>
+					))}
+				</ul>
+			</nav>
+		</header>
+	);
 };
 
 export default Header;
